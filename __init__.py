1436aa4b0b630c51fd2609c01a9c207684152db7
Rick Mak <rick.mak@gmail.com>
2016-12-14 14:21:26 +0800

    Add demo assets

--- __init__.py
+++ __init__.py
@@ -1,5 +1,11 @@
 from skygear.settings import settings
+from skygear import static_assets
+from skygear.utils.assets import relative_assets
 
 from .chat import includeme
 
 includeme(settings)
+
+@static_assets(prefix='demo')
+def chat_demo():
+    return relative_assets('chat-SDK-JS/demo')
